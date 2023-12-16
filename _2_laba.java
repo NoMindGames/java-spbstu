96c7a204d6ebd7488146363f5a7aad6846c7beca
NoMindGames <limkaseveral@gmail.com>
2023-12-16 19:34:31 +0300

    All homeworks done

---
+++ _2_laba.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class _2_laba {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str;
+        for (; scan.hasNext(); ) {
+            str =scan.next();
+            System.out.print(str + " ");
+        }
+    }
+}
